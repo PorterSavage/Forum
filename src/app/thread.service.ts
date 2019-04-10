942682484a6078983e1b3753402e1b82c0500a6e
Porter Savage <theportersavage@gmail.com>
2019-04-10 16:46:22 -0700

    Connected firebase to my app and now i'm working on creating new data and sending it from the app to the database

---
+++ src/app/thread.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Thread } from './models/createPost.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThreadService {
+
+  constructor() { }
+}
