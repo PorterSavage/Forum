9d7d3b2c69cc1123573630560cdfccbdd3c90d3a
Porter Savage <theportersavage@gmail.com>
2019-04-11 10:32:10 -0700

    Got the posts to show up on the main page after the user creates a thread

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ThreadService } from '../thread.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [ThreadService]
 })
 export class HomeComponent implements OnInit {
+  threads: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private threadService: ThreadService) { }
 
   ngOnInit() {
+    this.threads = this.threadService.getThreads()
   }
 
 }
